2a71cb38add32a47d667343bd95b5c82dbeb3d08
yahyes <yayadan2584@gmail.com>
2023-02-16 12:48:56 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include < stdio.h >
+/**
+ * main - using the function puts
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
