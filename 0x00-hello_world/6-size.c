dd24d3600baafe8b2302406c3bb67976c8fc9d4f
yahyes <yayadan2584@gmail.com>
2023-02-17 01:14:51 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("size of a long int: %ld byte(s)\n", (unsigned long)sizeof(c));
-printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("size of a long long int: %ld byte(s)\n", (unsigned long)sizeof(d));
+printf("size of a float: %ld byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
