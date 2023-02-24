fbc9f87e7e23ae4db93bc3d83ab11e3dbdd3bade
yahyes <yayadan2584@gmail.com>
2023-02-24 03:43:43 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -20,9 +20,17 @@ void print_line(int n)
 {
 	int i;
 
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
 	for (i = 0; i < n; i++)
 	{
-		_putchar('_');
+	_putchar('_');
 	}
 	_putchar('\n');
+	}
 }
+
