d877295eebbe422b52b67129264a512cec83835a
yahyes <yayadan2584@gmail.com>
2023-03-10 05:48:30 -0800

    1-memcpy.c

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_memcpy - function declaration
+ * @dest: destination address
+ * @src: source address
+ * @n: number of times
+ * Return: sring
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int index;
+
+	for (index = 0; index < n; index++)
+	{
+		dest[index] = src[index];
+	}
+
+	return (dest);
+}
