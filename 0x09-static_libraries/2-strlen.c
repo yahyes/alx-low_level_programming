50df13e2bdad538e2397a345238f67faf80d88b3
yahyes <yayadan2584@gmail.com>
2023-03-10 06:16:30 -0800

    2-strlen.c

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - find length of a string
+ * @s: the string ptr
+ * Return: index
+ */
+
+int _strlen(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0'; index++)
+	{
+	}
+
+	return (index);
+}
