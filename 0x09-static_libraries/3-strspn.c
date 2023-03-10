e419070ccc92742193284dad992595c9f9868fc8
yahyes <yayadan2584@gmail.com>
2023-03-10 06:48:21 -0800

    3-strspn.c

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strspn - function declaration
+ * @s: source string
+ * @accept: prefix substring
+ * Return: number of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int bytes = 0;
+	int index;
+
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+			{
+				bytes++;
+				break;
+			}
+			else if (accept[index + 1] == '\0')
+			{
+				return (bytes);
+			}
+		}
+		s++;
+	}
+	return (bytes);
+}
