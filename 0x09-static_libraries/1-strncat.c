ace3ed819adf3da41277259a67e95d6eeeed3f24
yahyes <yayadan2584@gmail.com>
2023-03-10 05:53:52 -0800

    1-strncat.c

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncat - function declaration
+ * @dest: second string
+ * @src: first string
+ * @n: number of time to append
+ * Return: character string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int src_index;
+	int dest_index;
+
+	for (dest_index = 0; dest[dest_index] != '\0'; dest_index++)
+	{}
+
+	for (src_index = 0; src_index < n && src[src_index] != '\0'; src_index++)
+	{
+		dest[dest_index + src_index] = src[src_index];
+	}
+	dest[dest_index + src_index] = '\0';
+
+	return (dest);
+}
