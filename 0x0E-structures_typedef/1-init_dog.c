0e21fa2eb2614a49bfb91dcb19b5c990f9a54a2e
yahyes <yayadan2584@gmail.com>
2023-03-21 20:11:47 +0300

    woofinit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - structure of the dog
+ * @d: address of the dog
+ * @name: name of the dog
+ * @age: age of the dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
