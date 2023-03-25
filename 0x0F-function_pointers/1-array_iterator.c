73e7416f3e1dda37d6c6a74b31c023f666ac91f9
yahyes <yayadan2584@gmail.com>
2023-03-25 15:55:06 +0300

    array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,7 +15,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array == NULL || action == NULL)
 		exit(98);
-	
+
 	for (index = 0; index < size; index++)
 	{
 		action(array[index]);
